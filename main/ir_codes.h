739eabc04af5cad76a84d110ce346b71b876d8e9
AhmadAC <cooperahmad@gmail.com>
2026-07-01 14:33:57 +0800

    refactored

---
+++ main/ir_codes.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+extern const uint8_t ir_off[];
+extern const size_t ir_off_len;
+
+extern const uint8_t ir_on[];
+extern const size_t ir_on_len;
+
+extern const uint8_t ir_19c[];
+extern const size_t ir_19c_len;
+
+extern const uint8_t ir_24c[];
+extern const size_t ir_24c_len;
